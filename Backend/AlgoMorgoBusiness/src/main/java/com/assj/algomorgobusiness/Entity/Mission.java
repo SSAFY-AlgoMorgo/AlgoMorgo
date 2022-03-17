d56d881d92a3f9bc16f72fe241cbf96ea43d1f38
lmw0122 <lamyunlee@naver.com>
2022-03-17 16:03:09 +0900

    b/feat/26

---
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/Entity/Mission.java
@@ -0,0 +1,36 @@
+package com.assj.algomorgobusiness.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "mission")
+public class Mission {
+
+    @Id
+    @Column(name = "mission_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long missionId;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @Column(name = "success_date")
+    private LocalDateTime successDate;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+
+}
