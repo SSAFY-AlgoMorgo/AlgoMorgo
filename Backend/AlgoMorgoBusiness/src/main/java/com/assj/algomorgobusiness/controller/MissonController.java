5ceb7e45f5ba3f611d897318665c7d5ba4e7b874
SSAFY <chlj1101@gmail.com>
2022-03-27 23:19:07 +0900

    b/feat/80 update mission service in business

--- Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/controller/MissonController.java
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/controller/MissonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -36,6 +37,12 @@ public class MissonController {
         List<MissionDto> result = missionService.getMissionByWeek(userId);
         log.info(result.toString());
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Map<String,Integer>> getMissionInfo(@PathVariable("userId") String userId){
+        Map<String, Integer> result = missionService.getMission(userId);
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
 
 }
--- Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/MissionRepository.java
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/MissionRepository.java
@@ -15,6 +15,7 @@ public interface MissionRepository extends JpaRepository<Mission,Long> {
 
     List<Mission> findAllByUserAndCreateDateBetween(User user, @Param("createDate") LocalDateTime start, @Param("successDate") LocalDateTime now);
 
+    List<Mission> findAllByUserAndSuccessDateIsNotNullOrderByCreateDate(User user);
 //    @Query(value = "select * from test.mission where id =:id and create_date between date_add(now(), INTERVAL :minus DAY) and now()", nativeQuery = true)
 //    List<Mission> findAllByUserUIdAndCreateDateBetween(@Param("id") int id, @Param("minus") int minus);
 
