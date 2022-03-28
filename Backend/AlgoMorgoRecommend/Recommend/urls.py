76ef685bb45a72caec380de05eecebc8c7a42908
lmw0122 <lamyunlee@naver.com>
2022-03-28 18:08:16 +0900

    b/feat/88 done

--- Backend/AlgoMorgoRecommend/Recommend/urls.py
+++ Backend/AlgoMorgoRecommend/Recommend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
 app_name = 'Recommend'
 urlpatterns = [
-    path('<str:userId>', views.recommendProblem),
+    path('one/<str:userId>', views.recommendProblemOne, name='recommend_problem_one'),
+    path('all',views.recommendProblemAll),
+    path('updatefiles',views.updateFiles)
 ]
