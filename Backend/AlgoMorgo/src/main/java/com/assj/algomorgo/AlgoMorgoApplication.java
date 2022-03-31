329debfd90951379b3bdeaafa9d2353d077c8400
SSAFY <chlj1101@gmail.com>
2022-03-31 14:36:13 +0900

    b/fear:98 connect batch to redis and redis to db

--- Backend/AlgoMorgo/src/main/java/com/assj/algomorgo/AlgoMorgoApplication.java
+++ Backend/AlgoMorgo/src/main/java/com/assj/algomorgo/AlgoMorgoApplication.java
@@ -2,8 +2,10 @@ package com.assj.algomorgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlgoMorgoApplication {
 
     public static void main(String[] args) {
