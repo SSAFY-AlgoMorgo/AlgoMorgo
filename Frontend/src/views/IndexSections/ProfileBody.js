95a385a9a05ceee16287148c738108af3e2fa22a
elmitricster <dpfmal55@naver.com>
2022-04-01 19:13:57 +0900

    f/hot fix

--- Frontend/src/views/IndexSections/ProfileBody.js
+++ Frontend/src/views/IndexSections/ProfileBody.js
@@ -22,50 +22,6 @@ function ProfileBody() {
                 <p className="h7 mt-3 ml-4 font-weight-bold"><i class="ni ni-single-02"></i> 회원 정보</p>
               </div>
 
-              {/* <Row className="py-3 align-items-center">
-                  <Col sm="3">
-                    <Button className="ml-6 col-7"
-                      block
-                      font-weight-bold
-                      color="neutral"
-                      disabled
-                      type="button">
-                      <h7>아이디</h7>
-                    </Button>
-                </Col>
-                  <Col sm="8">
-                    <Button
-                      block
-                      color="neutral"
-                      disabled
-                      type="button">
-                    <h7>ssafykim</h7>   
-                  </Button>
-                </Col>
-                </Row> */}
-
-              {/* <Row className="py-3 align-items-center">
-                <Col sm="3">
-                <Button className="ml-6 col-7"
-                      block
-                      font-weight-bold
-                      color="neutral"
-                      disabled
-                      type="button">
-                      <h7>비밀번호</h7>
-                    </Button>
-                </Col>
-                <Col sm="8">
-                <Button
-                      block
-                      color="neutral"
-                      disabled
-                      type="button">
-                    <h7>************</h7>   
-                  </Button>
-                </Col>
-              </Row> */}
-
               <Row className="py-3 align-items-center">
                 <Col sm="3">
                 <Button className="ml-6 col-7"
