7aca5dff7b6974f3fdbc020a4a2bc68cb531a32e
s6082648 <s6082648@naver.com>
2022-04-01 13:55:06 +0900

    b/feat/118 create redis service in business

--- Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/MissionRepository.java
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/MissionRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MissionRepository extends JpaRepository<Mission,Long> {
@@ -18,6 +19,5 @@ public interface MissionRepository extends JpaRepository<Mission,Long> {
     List<Mission> findAllByUserAndSuccessDateIsNotNullOrderByCreateDate(User user);
 //    @Query(value = "select * from test.mission where id =:id and create_date between date_add(now(), INTERVAL :minus DAY) and now()", nativeQuery = true)
 //    List<Mission> findAllByUserUIdAndCreateDateBetween(@Param("id") int id, @Param("minus") int minus);
-
 
 }
--- Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/RedisRepository.java
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/repository/RedisRepository.java
@@ -2,9 +2,11 @@ package com.assj.algomorgobusiness.repository;
 
 import com.assj.algomorgobusiness.dto.RedisDto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RedisRepository extends CrudRepository<RedisDto, String> {
 
 }
