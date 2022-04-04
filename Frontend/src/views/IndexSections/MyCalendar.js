ca3f0ba937c69cffd729033ac0950c9688c58dd8
lmw0122 <lamyunlee@naver.com>
2022-04-04 17:22:52 +0900

    add modal

--- Frontend/src/views/IndexSections/MyCalendar.js
+++ Frontend/src/views/IndexSections/MyCalendar.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import axios from 'axios';
 import '../../assets/css/Calendar.css';
-
+import Badge from 'reactstrap/lib/Badge';
+import {
+  Button,
+  Modal
+} from "reactstrap";
 function makeDate(y, m, d) {
   let result = "";
   result += y+"-";
@@ -15,10 +19,11 @@ function makeDate(y, m, d) {
   return result;
 }
 let count = false;
+let countForModal = false;
 function MyCalendar() {
   const [value, onChange] = useState(new Date());
   const [selectedMission, setSelectedMission] = useState([]);
-
+  const [visible, setVisible] = useState(false);
   let curTime = new Date();
   let curYear = curTime.getFullYear();
   let curMonth = curTime.getMonth() + 1;
@@ -67,11 +72,84 @@ function MyCalendar() {
     })
   }, [value]);
   useEffect(() => {
+    if (!countForModal) {
+      countForModal = true;
+      return;
+    }
     console.log(selectedMission);
+    setVisible(true);
   }, [selectedMission]);
   return (
     <div className='my-5'>
-      <Calendar onChange={onChange} value={value} calendarType="US"/>
+      <Calendar onChange={onChange} value={value} calendarType="US" />
+      <Modal
+        className="modal-dialog-centered"
+        isOpen={visible}
+        toggle={() => setVisible(prev => !prev)}
+      >
+        <div className="modal-header">
+          <h6 className="modal-title" id="modal-title-default">
+            {value.getFullYear()}년 {value.getMonth() + 1}월 {value.getDate()}일 미션들
+          </h6>
+          <button
+            aria-label="Close"
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setVisible(prev => !prev)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          {selectedMission.length === 0
+            ? "해당 날짜에 미션이 없습니다."
+            :
+            <table className='table-bordered' style={{ width: "100%" }}>
+              <thead>
+                <tr>
+                  <th>문제</th>
+                  <th>문제 제목</th>
+                  <th>정답</th>
+                  <th>정답 비율</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  selectedMission.map(mission =>
+                    <tr>
+                      <td>{mission.problemDto.problemNum}</td>
+                      <td>{mission.problemDto.problemName}</td>
+                      <td>
+                        {
+                          mission.successDate !== null
+                            ? <Badge className="text-uppercase ml-1" color="success" pill>O</Badge>
+                            : <Badge className="text-uppercase ml-1" color="danger" pill>
+                            x
+                          </Badge>
+      
+                      }
+                      </td>
+                      <td>{mission.problemDto.problemAnswer}</td>
+                    </tr>
+                    )
+                }
+              </tbody>
+            </table>
+          }
+        </div>
+        <div className="modal-footer">
+          <Button
+            className="ml-auto"
+            color="link"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setVisible(prev => !prev)}
+          >
+            닫기
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 }
