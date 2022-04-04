d8c3a811bb5f9845675404f51443aa9c92cf6786
s6082648 <s6082648@naver.com>
2022-04-04 14:10:23 +0900

    b/feat/118 create redis service in business update

--- Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/service/redis/RedisServiceImpl.java
+++ Backend/AlgoMorgoBusiness/src/main/java/com/assj/algomorgobusiness/service/redis/RedisServiceImpl.java
@@ -4,8 +4,10 @@ import com.assj.algomorgobusiness.dto.MissionDto;
 import com.assj.algomorgobusiness.dto.ProblemDto;
 import com.assj.algomorgobusiness.dto.RedisDto;
 import com.assj.algomorgobusiness.entity.Problem;
+import com.assj.algomorgobusiness.entity.User;
 import com.assj.algomorgobusiness.repository.ProblemRepository;
 import com.assj.algomorgobusiness.repository.RedisRepository;
+import com.assj.algomorgobusiness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ public class RedisServiceImpl implements RedisService{
 
     private final RedisRepository redisRepository;
     private final ProblemRepository problemRepository;
+    private final UserRepository userRepository;
 
     @Override
     public RedisDto save(RedisDto redisDto) {
@@ -31,8 +34,8 @@ public class RedisServiceImpl implements RedisService{
     public List<MissionDto> getUserMission(String Id) { // 주석 확인, yml
 
         // userId 받아서 pk Id 가져오기
-//        Optional<User> user = userRepository.findByUserId(Id);
-//        String userId = user.get().getId() + "";
+        Optional<User> user = userRepository.findByUserId(Id);
+        String userId = user.get().getId() + "";
         RedisDto redisDto = redisRepository.findById(Id).orElseThrow(() -> new IllegalArgumentException("뭐가 문제야!!!!!!!!!!!!!!!!!!!!!"));
         List<MissionDto> missionDtoList = new ArrayList<>();
 
@@ -44,6 +47,8 @@ public class RedisServiceImpl implements RedisService{
     @Override
     public List<MissionDto> getRefreshMission(String Id) {
 
+        Optional<User> user = userRepository.findByUserId(Id);
+        String userId = user.get().getId() + "";
         RedisDto redisDto = redisRepository.findById(Id).orElseThrow(() -> new IllegalArgumentException("뭐가 문제야!!!!!!!!!!!!!!!!!!!!!"));
         List<MissionDto> missionDtoList = new ArrayList<>();
         List<Integer> trueNum = new ArrayList<>();
